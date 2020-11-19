8bd6264aeb947abd17a517c9d62f8260a34964ad
Dianhanluosi <evanzheng1127@gmail.com>
2020-11-19 07:25:47 -0800

    Example Script change

--- Assets/reprotestasset.cs
+++ Assets/reprotestasset.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class reprotestasset : MonoBehaviour
 {
+
+    public bool x;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //bababooey
     }
 
     // Update is called once per frame
